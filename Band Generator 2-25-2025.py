2ecba806b55c13d2f76bb615e4aad7dd95d266b9
Kyle <kateuptonishot77@gmail.com>
2025-02-25 20:12:28 -0600

    Create Band Generator 2-25-2025.py

---
+++ Band Generator 2-25-2025.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("Enter the city you grew up in.\n")
+pet = input ("Now enter the name of a pet.\n")
+print("Your band name could be: " + city + " " + pet)
