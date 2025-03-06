d508a27020374f617828cf11a52bf1fa86ddf380
Kyle <kateuptonishot77@gmail.com>
2025-03-05 18:27:11 -0600

    Art and Caesar Cipher
    The art is for the Caesar cipher. Which allows you to create your own coded messages.

---
+++ Caesar Cipher.py
@@ -0,0 +1,43 @@
+# TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(logo)
+# TODO-2: What happens if the user enters a number/symbol/space?
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    if encode_or_decode == "decode":
+
+        shift_amount = abs(shift_amount) * -1
+
+    elif encode_or_decode != "encode" or "decode":
+        encode_or_decode = "encode"
+
+    for letter in original_text:
+
+       if letter not in alphabet:
+         output_text += letter
+
+       else:
+
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+
+# TODO-3: Can you figure out a way to restart the cipher program?
+end_program = False
+while not end_program:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    user_continue = input("Do you to continue? N for no or hit any button to continue.\n").lower()
+    if user_continue == "n":
+        end_program = True
+
